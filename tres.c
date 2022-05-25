bbd600fc57534fda5bfed2b4d85ddb624851572a
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-25 03:01:14 -0300

    Create tres.c

---
+++ tres.c
@@ -0,0 +1,26 @@
+/*Escreva um procedimento que receba um número inteiro e imprima o
+mês correspondente ao número. Por exemplo, 2 corresponde à
+“fevereiro”. O procedimento deve mostrar uma mensagem de erro caso
+o número recebido não faça sentido. Faça um programa que leia um
+valor e chame o procedimento criado.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mesesDoAno(int n);
+
+int main(){
+    int n=0;
+    printf("Digite o número correspondente ao mes: ");
+    scanf("%d", &n);
+    mesesDoAno(n);
+    return 0;
+}
+
+void mesesDoAno(int n){
+    char meses[12][10]= {"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho",
+    "agosto", "setembro", "outubro", "novembro", "dezembro"};
+
+    printf("%s", meses[n-1]);
+
+}
