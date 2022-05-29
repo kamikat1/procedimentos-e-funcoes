741dd18be356795ed1239f4944c401f397a79e07
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-29 03:27:12 -0300

    Create sete.c

---
+++ sete.c
@@ -0,0 +1,31 @@
+/*Faça uma função que receba três valores e retorne o maior.*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+float retornaMaior (float a, float b, float c);
+
+int main (){
+float result=0;
+float a, b, c;
+int i;
+
+    printf("Digite os 3 valores:");
+    scanf("%f %f %f", &a, &b, &c);
+
+    result = retornaMaior (a,b,c);
+    printf("%f",retornaMaior);
+
+    return 0;
+}
+
+float retornaMaior (float a, float b, float c) {
+
+    if (a>b && a>c)
+        return a;
+    else if(b>a && b>c)
+        return b;
+    else if(c>a && c>b)
+        return c;
+}
+
